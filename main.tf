5d915ea7140ccf2213dff837ed057beb1a1bac04
Agung Gunawan <agung.one94@gmail.com>
2022-05-03 16:21:07 +0700

    Update TF Structure

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "my-training-sf"
-  region  = "asia-southeast2"
-  zone    = "asia-southeast2-a"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
---
+++ variable.tfvars
@@ -0,0 +1,3 @@
+project_id = "my-training-sf"
+region     = "asia-southeast2"
+zone       = "asia-southeast2-a"
